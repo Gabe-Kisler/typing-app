5cfa5aab4fd81eaa853090ab7814609491eeb0ee
Gabe <gabrielkisler@earthlink.net>
2025-03-28 22:04:13 -0500

    services refinement

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, request, jsonify, session
-from .services import load_words_from_csv, get_total_test_taken, store_stats, get_best_test, get_average_tests, get_total_time_typing
+from .services import load_words_from_csv, get_total_test_taken, store_stats, get_best_test, get_average_tests, get_total_time_typing, get_tests
+
+
 
 
 routes_bp = Blueprint ('routes_bp', __name__)
@@ -8,18 +10,25 @@ def index():
     return render_template ('index.html')
 
 
+
+
 @routes_bp.route('/launch')
 def launch():
     return render_template('launch.html')
+
 
 @routes_bp.route('/stats-render')
 def render_stats():
     return render_template('stats.html')
 
 
+
+
 @routes_bp.route ('/get-words', methods=['POST', 'GET'])
 def get_words():
     level_choice = request.get_json().get('level')
+
+
 
 
     word_files = {
@@ -29,18 +38,25 @@ def get_words():
     }
 
 
+
+
     words_csv = word_files.get (level_choice, 'app/static/resources/easyWords.csv')
     random_words = load_words_from_csv (words_csv)
 
 
+
+
     return jsonify (wordsString=' '.join(random_words))
+
 
 @routes_bp.route('/test-finished', methods=['POST'])
 def store_user_stats ():
     user_id = session.get('user_id')
     data = request.get_json()
 
+
     print (data)
+
 
     net_wpm = data.get('netWPM')
     gross_wpm = data.get('grossWPM')
@@ -48,25 +64,31 @@ def store_user_stats ():
     time = data.get('time')
     difficulty = data.get('difficulty')
 
+
     test_taken = get_total_test_taken(user_id, None) + 1
     test_id = store_stats(user_id, test_taken, net_wpm, gross_wpm, errors, time, difficulty)
     return jsonify ({'succesfully stored test, tes_id:':test_id})
 
+
 @routes_bp.route('/retrieve-stats', methods=['POST'])
 def get_user_stats ():
     user_id = session.get('user_id')
-    total_tests = get_total_test_taken(user_id, None)
-    typing_time = get_total_time_typing(user_id)
-    best_ovr_wpm = get_best_test (user_id, None)
-    fifteen_sec_best = get_best_test (user_id, 15)
-    fifteen_sec_avg = get_average_tests (user_id, 15)
-    fifteen_sec_tests = get_total_test_taken (user_id, 15)
-    thirty_sec_best = get_best_test (user_id, 30)
-    thirty_sec_avg = get_average_tests (user_id, 30)
-    thirty_sec_tests = get_total_test_taken (user_id, 30)
-    sixty_sec_best = get_best_test (user_id, 60)
-    sixty_sec_avg = get_average_tests (user_id, 60)
-    sixty_sec_tests = get_total_test_taken (user_id, 60)
+    tests = get_tests (user_id)
+
+
+    total_tests = get_total_test_taken(None, tests)
+    typing_time = get_total_time_typing(tests)
+    best_ovr_wpm = get_best_test (None, tests)
+    fifteen_sec_best = get_best_test (15, tests)
+    fifteen_sec_avg = get_average_tests (15, tests)
+    fifteen_sec_tests = get_total_test_taken (15, tests)
+    thirty_sec_best = get_best_test (30, tests)
+    thirty_sec_avg = get_average_tests (30, tests)
+    thirty_sec_tests = get_total_test_taken (30, tests)
+    sixty_sec_best = get_best_test (60, tests)
+    sixty_sec_avg = get_average_tests (60, tests)
+    sixty_sec_tests = get_total_test_taken (60, tests)
+
 
     return jsonify ({
         'total_tests': total_tests,
@@ -82,6 +104,13 @@ def get_user_stats ():
         'sixty_sec_avg': sixty_sec_avg,
         'sixty_sec_tests': sixty_sec_tests
     })
+
+
+
+
+
+
+
 
 
 
--- app/services.py
+++ app/services.py
@@ -1,8 +1,14 @@
-import csv, random
+import csv, random, math
 from firebase_admin import firestore, auth
 
 
+
+
+
+
 db = firestore.client()
+
+
 
 
 def load_words_from_csv (csv_file):
@@ -14,6 +20,8 @@ def load_words_from_csv (csv_file):
             words.append(row[0])
 
 
+
+
         random_words = random.sample (words, 200)
         return random_words
    
@@ -21,6 +29,8 @@ def create_firebase_user (username, email, password):
     user = auth.create_user(email=email, password=password)
     db.collection('users').document(user.uid).set({'username':username, 'email':email})
     return user
+
+
 
 
 def store_stats (user_id, test_taken, net_wpm, gross_wpm, errors, time, difficulty):
@@ -36,79 +46,92 @@ def store_stats (user_id, test_taken, net_wpm, gross_wpm, errors, time, difficul
     })
     return test.id
 
-def get_total_test_taken (user_id, time):
+
+def get_tests (user_id):
+    tests_ref = db.collection('users').document(user_id).collection('tests').stream()
+    tests = []
+    for test in tests_ref:
+        tests.append (test.to_dict())
+
+
+    return tests    
+
+
+def get_total_test_taken (time, tests):
     total_tests = 0
-    tests = db.collection('users').document(user_id).collection('tests').stream()
+
 
     if (time == None):
-        total_tests = len(list(tests))
-    elif (time != None):
+        return len(tests)
+    else:
         for test in tests:
-            test_doc = test.to_dict()
-            test_time = test_doc.get('time', 0)
-            if int(time) == int(test_time):
+            test_time = test.get('time', 0)
+            if time == int(test_time):
                 total_tests += 1
-    return total_tests
+        return total_tests
 
-def get_total_time_typing (user_id):
-    tests = db.collection('users').document(user_id).collection('tests').stream()
+
+def get_total_time_typing (tests):
     total_time = 0
-
     for test in tests:
-        test_doc = test.to_dict()
-        time = test_doc.get('time', 0)
+        time = test.get('time', 0)
         total_time += int(time)
+    total_time_secs = (total_time % 60)
+    total_time_mins = math.floor(total_time / 60)
 
-    total_time_mins = round (total_time / 60, 2)
-    return total_time_mins
-    
-def get_best_test (user_id, time):
-    tests = db.collection('users').document(user_id).collection('tests').stream()
+
+    return str(total_time_mins) + ":" + str(total_time_secs)
+   
+def get_best_test (time, tests):
     best_wpm = 0
 
-    if not tests:
-        print ("no tests for user")
 
     for test in tests:
-        test_doc = test.to_dict()
-        wpm = test_doc.get('gross_wpm', 0)
-        test_time = test_doc.get('time', 0)
-        print (test_time, wpm)
+        wpm = test.get('gross_wpm', 0)
+        test_time = test.get('time', 0)
+
 
         if (time == None):
-            if int(wpm) > int(best_wpm):
-                best_wpm = wpm
-        
-        elif int(time) == int(test_time):
-            if int(wpm) > int(best_wpm):
-                best_wpm = wpm
+            if int(wpm) > best_wpm:
+                best_wpm = float(wpm)
+       
+        elif time == int(test_time):
+            if int(wpm) > best_wpm:
+                best_wpm = float(wpm)
 
 
-    return best_wpm 
-  
-def get_average_tests (user_id, time):
-    tests = db.collection('users').document(user_id).collection('tests').stream()
+
+
+    return round (best_wpm, 1)
+ 
+def get_average_tests (time, tests):
     wpm_total = 0
     count = 0
 
+
     for test in tests:
-        test_doc = test.to_dict()
-        wpm = test_doc.get('gross_wpm', 0)
-        test_time = test_doc.get('time', 0)
+        wpm = test.get('gross_wpm', 0)
+        test_time = test.get('time', 0)
+
 
         if (time == None):
-            wpm_total += wpm
+            wpm_total += int(wpm)
             count += 1
-        
-        elif int(time) == int(test_time):
-            wpm_total += wpm
+       
+        elif time == int(test_time):
+            wpm_total += int(wpm)
             count += 1
 
+
     return round(wpm_total / count, 2) if count > 0 else 0
+
+
 
 
 def send_user_stats (user_id):
     total_test_taken = get_total_test_taken(user_id)
     fifteen_best = get_best_test (user_id, 15)
+
+
 
 
